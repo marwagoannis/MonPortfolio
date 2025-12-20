0f4c1ff448133bf666c56e4d9e3a815cfebb464a
marwagoannis <marwag.pro@outlook.fr>
2025-12-20 11:43:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const btn = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme');
 
+// 1. Vérifie si un thème est déjà enregistré dans le navigateur
+if (currentTheme === 'dark') {
+    document.body.classList.add('dark-theme');
+    btn.textContent = '☀️';
+}
+
+// 2. Gère le clic sur le bouton
 btn.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
     
-    // Change l'icône selon le mode
+    let theme = 'light';
     if (document.body.classList.contains('dark-theme')) {
+        theme = 'dark';
         btn.textContent = '☀️';
     } else {
         btn.textContent = '🌙';
     }
+    
+    // 3. Enregistre le choix dans le localStorage
+    localStorage.setItem('theme', theme);
 });
